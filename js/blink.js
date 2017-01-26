0481fd1eb2de4c00c349caefd994e87db8d8c8b6
Chrisis824 <p14149479@my365.dmu.ac.uk>
2017-01-26 15:18:54 +0000

    changed the home page top menu and the image slider. Added some more styling such as actions to happen when hovering over a text or an image

--- js/blink.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    var f = document.getElementById('roww');
-    setInterval(function() {
-        f.style.display = (f.style.display == 'none' ? '' : 'none');
-    }, 1000);
-
-});
